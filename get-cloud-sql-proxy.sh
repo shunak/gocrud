1b9110bb100eb0ab90aa88ef8426117c52214dc5
shunak <shuna966@gmail.com>
2020-11-13 15:33:36 +0900

    modified get-cloud-sql-proxy.sh

--- get-cloud-sql-proxy.sh
+++ get-cloud-sql-proxy.sh
@@ -1,4 +1,4 @@
-# downloading cloud_sql_proxy
+# downloading cloud_sql_proxy for macos64bit. other OS types here https://cloud.google.com/sql/docs/mysql/sql-proxy#install 
 curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
 # make runable cloud_sql_proxy
 chmod +x cloud_sql_proxy
