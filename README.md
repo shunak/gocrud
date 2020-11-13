808c9a761166d97179ecb9326f01c815437aecb5
shunak <shuna966@gmail.com>
2020-11-13 15:23:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# gocrud
+
+This is a simple go CRUD application, connected with  
+Now Editing...
---
+++ get-cloud-sql-proxy.sh
@@ -0,0 +1,4 @@
+# downloading cloud_sql_proxy
+curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
+# runable cloud_sql_proxy
+chmod +x cloud_sql_proxy
